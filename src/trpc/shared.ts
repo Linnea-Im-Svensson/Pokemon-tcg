acf045e552f05388d4fa50a1f4b954f1824fb252
ChasAcademy-Linnea-Svensson <113734152+ChasAcademy-Linnea-Svensson@users.noreply.github.com>
2023-11-12 03:22:01 +0100

    changed base url

--- src/trpc/shared.ts
+++ src/trpc/shared.ts
@@ -8,7 +8,9 @@ export const transformer = superjson;
 function getBaseUrl() {
   if (typeof window !== "undefined") return "";
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
-  return `http://localhost:${process.env.PORT ?? 3000}`;
+  return `https://pokemon-4at72foe0-chasacademy-linnea-svensson.vercel.app/:${
+    process.env.PORT ?? 3000
+  }`;
 }
 
 export function getUrl() {
